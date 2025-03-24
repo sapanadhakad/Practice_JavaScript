8de7ed2a8b64d17c1635a18076de0d3a8e1a7f02
sapanadhakad <122146805+sapanadhakad@users.noreply.github.com>
2025-03-24 13:04:57 +0000

    commit

---
+++ basic/num.js
@@ -0,0 +1,4 @@
+console.log(Math.random())
+console.log(Math.floor(Math.random())
+)
+console.log(Math.floor(Math.random()*10))
---
+++ basic/strings.js
@@ -0,0 +1,13 @@
+const str1 = new String ("sapana")
+console.log(str1);
+console.log(typeof str1)
+const st =  "      hello"
+console.log(st);
+console.log(typeof st);
+console.log(st.charAt(4));
+console.log(str1.charAt(4));
+console.log(st.trim())
+console.log(str1.replace("a"," _ "))
+console.log(str1.includes("n"))
+console.log(str1.split("a",))
+console.log(`${st} miss ${str1}`);
